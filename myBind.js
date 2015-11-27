698cb8ff3ae9da274d9f7df26a98e4eb0ff2b5c3
App Academy Student <student@appacademy.io>
2015-11-27 14:27:51 -0800

    my Bind

--- myBind.js
+++ myBind.js
@@ -1,3 +1,18 @@
 Function.prototype.myBind = function(context) {
-
+  var fn = this;
+  return function() {
+    fn.apply(context);
+  };
 };
+
+function Cat (name) {
+  this.name = name;
+}
+
+var cat = new Cat("Gizmo");
+
+Cat.prototype.catName = function() {
+  console.log(this.name);
+};
+
+setTimeout(cat.catName.myBind(cat), 1000);
