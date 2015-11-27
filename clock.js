ceff1e8787c19b8e0eeb6981c018070e9d2af0ab
App Academy Student <student@appacademy.io>
2015-11-27 11:58:55 -0800

    clock

--- clock.js
+++ clock.js
@@ -1,21 +1,33 @@
 function Clock () {
 }
 
-Clock.TICK = 5000;
+var TICK = 1000;
 
-Clock.prototype.printTime = function () {
+Clock.prototype.printTime = function (time) {
   // Format the time in HH:MM:SS
+  var hours = time.getHours();
+  var minutes = time.getMinutes();
+  var seconds = time.getSeconds();
+  console.log(hours + ":" + minutes + ":" + seconds );
 };
 
 Clock.prototype.run = function () {
   // 1. Set the currentTime.
+  var time = new Date();
   // 2. Call printTime.
+ this.printTime(time);
   // 3. Schedule the tick interval.
+
+  var clockTick = this._tick.bind(this, time);
+
+  setInterval(clockTick, TICK);
 };
 
-Clock.prototype._tick = function () {
+Clock.prototype._tick = function (time) {
   // 1. Increment the currentTime.
+  time.setMilliseconds(time.getMilliseconds() + TICK);
   // 2. Call printTime.
+  this.printTime(time);
 };
 
 var clock = new Clock();
